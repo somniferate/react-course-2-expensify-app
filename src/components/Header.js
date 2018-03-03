75b87b4434cc0d47fbc8f471ee7045c436a7facf
Tom Waardenburg <tom@waard.net>
2018-03-03 13:05:30 +0100

    Moved summary to ExpenseDashboard component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { NavLink } from "react-router-dom"
-import ExpensesSummary from "./ExpensesSummary"
 
 
 const Header = () => (
@@ -8,7 +7,6 @@ const Header = () => (
         <h1>Expensify</h1>
         <NavLink to="/" activeClassName="is-active" exact={true}>Dashboard</NavLink>
         <NavLink to="/create" activeClassName="is-active">Create Expense</NavLink>
-        <ExpensesSummary />
     </header>
 );
 
